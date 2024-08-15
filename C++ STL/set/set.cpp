9e1f529587657f47c534797905c2fcb8b1fd7217
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-15 19:32:26 +0530

    add find and erase

--- C++ STL/set/set.cpp
+++ C++ STL/set/set.cpp
@@ -21,6 +21,27 @@ void explainSet()
   }
   cout << endl;
 
+  // erase in set
+  cout << "erase 3" << endl;
+  st.erase(3);
+  cout << "size of set: " << st.size() << endl;
+  for (auto it : st)
+  {
+    cout << it << " ";
+  }
+  cout << endl;
+  cout << "erase complete" << endl;
+
+  // find
+  if (st.find(9) != st.end())
+  {
+    cout << "Found" << endl;
+  }
+  else
+  {
+    cout << "Not found" << endl;
+  }
+
   auto i = st.find(9);
   if (i != st.end())
   {
