b05af8cd276222395e09f6ffb87581dea5b1ac12
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-18 13:44:50 +0530

    complete accumulate

--- Code.cpp
+++ Code.cpp
@@ -1,35 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void explainSort()
+void explainAccumulate()
 {
-  int arr[5] = {5, 23, 5, 56, 1};
-
-  sort(arr + 3, arr + 5);
-
-  // for (int i = 0; i < sizeof(arr); i++)
-  // {
-  //   cout << arr[i] << " ";
-  // }
-
+  int arr[5] = {4, 5, 3, 2, 1};
+  int sum = accumulate(arr, arr + sizeof(arr), 0);
+  cout << "Sum of: ";
   for (int i : arr)
   {
-    cout << i << " ";
+    cout << i << " +" << " ";
   }
-
-  cout << endl;
-
-  vector<int> vec = {9, 12, 99, 1, 20, 340, 11, 10, 933, 344, 10000, 2};
-  sort(vec.begin(), vec.begin() + 4);
-  for (auto it : vec)
-  {
-    cout << it << " ";
-  }
-  cout << endl;
+  cout << "0 = ";
+  cout << sum << endl;
 }
 
 int main()
 {
-  explainSort();
+  // accumulate
+  explainAccumulate();
   return 0;
 }
